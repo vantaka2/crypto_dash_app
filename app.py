0392c4fa1ab2c79c36126c81ab1c71ab445cf6af
keerthan <keerthanvantakala@gmail.com>
2018-03-11 09:18:10 -0500

    removing dash table

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_table_experiments as dt
 import pandas as pd
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output
